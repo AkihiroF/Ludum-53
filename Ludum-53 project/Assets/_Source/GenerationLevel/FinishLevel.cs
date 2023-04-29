2fc1ba1040511f44cab56302b5925dbd8e94fe01
Kulikov Valery <valenok801@gmail.com>
2023-04-29 19:20:44 +0300

    Add generation location

---
+++ Ludum-53 project/Assets/_Source/GenerationLevel/FinishLevel.cs
@@ -0,0 +1,19 @@
+using System;
+using _Source.GenerationLevel.PartsLevel;
+using UnityEngine;
+
+namespace _Source.GenerationLevel
+{
+    public class FinishLevel : MonoBehaviour
+    {
+        [SerializeField] private LayerMask layerInteractable;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if ((layerInteractable.value & (1 << other.gameObject.layer)) > 0)
+            {
+                var part = other.GetComponent<APartLevel>();
+                part.PlayerExit();
+            }
+        }
+    }
+}
